3773d6c0e4a78ec7cfe85b975ff5989a6446c1f6
shernoble <uranium716@gmail.com>
2023-06-30 17:57:05 +0530

    edited form by adding resp form module

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,7 +1,7 @@
-import { Component} from '@angular/core';
-import { User } from 'src/app/User';
+import { Component,OnDestroy,OnInit} from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
-import { FormControl,Validators } from '@angular/forms';
+import { FormControl,FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { Subject, switchMap, takeUntil } from 'rxjs';
 
 
 @Component({
@@ -9,57 +9,51 @@ import { FormControl,Validators } from '@angular/forms';
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit  {
 
-  nameControl: FormControl = new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]);
-  emailControl : FormControl = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')]);
+  formData: FormGroup;
+
+  private unsubscribe = new Subject<void>()
 
 
-  users : User[]=[];
+  constructor(private userService : UserService,private formBuilder: FormBuilder){}
 
-  name:string;
-  email:string;
-  gender:string = 'male';
-  status:boolean = false;
+  ngOnInit(): void {
+    this.formData = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      gender: ['male', Validators.required],
+      status: [false]
+    });
 
-  constructor(private userService : UserService){}
-
-  onSubmit(){
-    // do basic validation here -client side?
-    // name validate: only alphabets n space
-    //email: valid email and not in unique
     
 
-    if(!this.name) {
-      alert("please enter your name");
-      return;
+  }
+//   ngOnDestroy() {
+//     this.unsubscribe.next()
+// }
+
+  submitForm() {
+    if (this.formData.valid) {
+      // Handle form submission logic here
+      this.formData.value.status=this.formData.value.status?"active":"inactive";
+      let newUser=this.formData.value;
+      try{
+        this.userService.addUser(newUser).subscribe();
+
+      }
+      catch(error){
+        console.log(error);
+        
+      }
+      // console.log(this.formData.value);
+    } 
+    else {
+      // Form is invalid, display error messages or take appropriate action
+      alert("Error : invalid data");
     }
-    if(!this.email){
-      alert("please enter your email");
-      return ;
-    }
-    
 
-    const newUser={
-      name : this.name,
-      email: this.email,
-      gender : this.gender,
-      status: this.status? "active":"inactive"
-    }
-    
-    
-    this.userService.addUser(newUser).subscribe((newUser) => {
-      this.users.push(newUser);
-    })
-    
-
-    this.name='';
-    this.email='';
-    this.gender='male';
-    this.status=false;
-
-
-
+    this.formData.reset();
 
   }
 
