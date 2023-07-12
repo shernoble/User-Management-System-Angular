c6cc748d039e9c3268c4347cac1364ed7b4e1f03
shernoble <uranium716@gmail.com>
2023-07-12 15:37:14 +0530

    added env files to repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:true,
+    bearer_token:'Bearer 077e7fb9c550c5b6d718c4ef76112ac04b6bb25960ab27a563b30f714b70fe09'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production:false,
+    bearer_token:'Bearer 077e7fb9c550c5b6d718c4ef76112ac04b6bb25960ab27a563b30f714b70fe09'
+};
